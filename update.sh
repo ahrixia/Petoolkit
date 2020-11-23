eebe7940489229381cb4d0f78aea2cba45770134
Astik Rawat <35935843+ahrixia@users.noreply.github.com>
2020-11-23 17:08:19 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git clone https://github.com/ahrixia/Petoolkit
 sudo chmod +x Petoolkit/install.sh
+sudo chmod +x Petoolkit/uninstall.sh
+sudo chmod +x Petoolkit/update.sh
+sudo chmod +x Petoolkit/petoolkit.py
 bash Petoolkit/install.sh
